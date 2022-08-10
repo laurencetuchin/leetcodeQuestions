7cfb5edf1acbe79eb62e03f93bb11e4ae6591fd0
laurencetuchin <laurencetuchin@gmail.com>
2022-08-10 14:33:45 +1000

    Better BinarySearch solution

--- src/problems/BinarySearch704.java
+++ src/problems/BinarySearch704.java
@@ -2,12 +2,35 @@ package problems;
 
 public class BinarySearch704 {
     // brute force solution
+//    public int search(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == target) {
+//                return i;
+//            }
+//        } return -1;
+//    }
+
+    // better solution using binary search
     public int search(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                return i;
+        // need low, mid, high ranges
+        int low = 0;
+        int high = nums.length -1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
-        } return -1;
+        }
+        if (nums[low] == target) {
+            return low;
+        }
+        return -1;
     }
+
 
 }
