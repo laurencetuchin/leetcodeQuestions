03757b345be7ed6e589bced0a2ac85bff36a1290
laurencetuchin <laurencetuchin@gmail.com>
2022-08-10 20:13:52 +1000

    Count Odd numbers in range

---
+++ src/problems/CountOddNumbersIntervalRange1523.java
@@ -0,0 +1,11 @@
+package problems;
+
+public class CountOddNumbersIntervalRange1523 {
+
+    public int countOdds(int low, int high) {
+        if (low % 2 == 0 && high % 2 == 0) {
+            return (high - low) / 2;
+        }
+        return (high - low) / 2 + 1;
+    }
+}
