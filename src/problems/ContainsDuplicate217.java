1d4fe44287e9447a81b30102c5db9dbcd4182e4c
laurencetuchin <laurencetuchin@gmail.com>
2022-08-10 16:34:59 +1000

    ContainsDuplicate217 += forloop

--- src/problems/ContainsDuplicate217.java
+++ src/problems/ContainsDuplicate217.java
@@ -6,20 +6,22 @@ import java.util.Set;
 public class ContainsDuplicate217 {
 
     // brute force solution
-    public boolean containsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i +1; j < nums.length; j++) {
-                if (nums[j] == nums[i]) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
+//    public boolean containsDuplicate(int[] nums) {
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i +1; j < nums.length; j++) {
+//                if (nums[j] == nums[i]) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+
+
 
     // better solution using HashSet
 
-    Set<Integer> foundDuplicate = new HashSet<Integer>();
+    HashSet<Integer> foundDuplicate = new HashSet<Integer>();
     for (int i : nums) {
         if (foundDuplicate.contains(i)) return true;
         foundDuplicate.add(i);
@@ -27,6 +29,16 @@ public class ContainsDuplicate217 {
     return false;
 
 
+    // using for loop faster
+    public boolean containsDuplicate(int[] nums) {
+
+    HashSet<Integer> uniqueNumbers = new HashSet<>();
+    for (int i=0; i < nums.length; i++) {
+        if (uniqueNumbers.contains(nums[i])) return true;
+        uniqueNumbers.add(nums[i]);
+    }
+    return false;
+    }
 
 
 }
