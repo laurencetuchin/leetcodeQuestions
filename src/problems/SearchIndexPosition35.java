4a518da6f5b3d12c8033a5c4c1e9bab6eb1dbda1
laurencetuchin <laurencetuchin@gmail.com>
2022-08-10 15:17:29 +1000

    SearchIndexPosition35 binary search o log n

---
+++ src/problems/SearchIndexPosition35.java
@@ -0,0 +1,27 @@
+package problems;
+
+public class SearchIndexPosition35 {
+    public int searchInsert(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        if (nums[low] == target) {
+            return low;
+        }
+        return -1;
+    }
+
+
+
+}
