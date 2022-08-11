5e30d0d05a6305097f4d922956db21efaa7af50d
laurencetuchin <laurencetuchin@gmail.com>
2022-08-11 21:56:20 +1000

    TwoSum two pointer solution

--- src/problems/TwoSum1.java
+++ src/problems/TwoSum1.java
@@ -14,6 +14,36 @@ public class TwoSum1 {
         }
     }
         return new int[] {};
+
+    // twoSum using two Pointer Solution
+    Arrays.sort(nums);
+    int pointer_one = 0;
+    int pointer_two = nums.length - 1;
+    // int[] result = new int[2];
+
+        while (pointer_one < pointer_two)
+
+    {
+        // int sum = nums[pointer_one] + nums[pointer_two];
+
+        if (nums[pointer_one] + nums[pointer_two] == target) {
+            // result[0] = pointer_one;
+            // result[1] = pointer_two;
+            return new int[]{pointer_one, pointer_two};
+            // return result;
+        } else if (nums[pointer_one] + nums[pointer_two] < target) {
+            pointer_one++;
+        } else {
+            pointer_two--;
+        }
+    }
+
+    }
+        return new int[] {} ;
+
+
+}
+
 }
 
 
