6c31fd7e874a7821015424f3b0d7802540841ba2
laurencetuchin <laurencetuchin@gmail.com>
2022-08-12 13:34:16 +1000

    TwoSum HashMap Solution

--- src/problems/TwoSum1.java
+++ src/problems/TwoSum1.java
@@ -1,7 +1,9 @@
 package problems;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Hashtable;
 
 public class TwoSum1 {
 
@@ -15,7 +17,27 @@ public class TwoSum1 {
     }
         return new int[] {};
 
-    // twoSum using two Pointer Solution
+
+    // Using HashMap
+
+    public int[] twoSum(int[] nums, int target) {
+
+        int[] answer = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(target - nums[i],i);
+            } else {
+                answer[0] = i;
+                answer[1] = map.get(nums[i]);
+                break;
+            }
+        }
+        return answer;
+
+
+        // twoSum using two Pointer Solution
     Arrays.sort(nums);
     int pointer_one = 0;
     int pointer_two = nums.length - 1;
@@ -29,7 +51,7 @@ public class TwoSum1 {
         if (nums[pointer_one] + nums[pointer_two] == target) {
             // result[0] = pointer_one;
             // result[1] = pointer_two;
-            return new int[]{pointer_one, pointer_two};
+            return new int[] {pointer_one, pointer_two};
             // return result;
         } else if (nums[pointer_one] + nums[pointer_two] < target) {
             pointer_one++;
@@ -43,6 +65,7 @@ public class TwoSum1 {
 
 
 }
+
 
 }
 
