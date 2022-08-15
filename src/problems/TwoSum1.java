5f6e2e3b64f91ab7c1599d6d955dd72dc3b89ad8
laurencetuchin <laurencetuchin@gmail.com>
2022-08-15 15:05:55 +1000

    refactor renaming for two pointer solution

--- src/problems/TwoSum1.java
+++ src/problems/TwoSum1.java
@@ -40,20 +40,20 @@ public class TwoSum1 {
         // twoSum using two Pointer Solution
         // requires array to be sorted before using two pointers
         Arrays.sort(nums);
-        int pointer_one = 0;
-        int pointer_two = nums.length - 1;
+        int start = 0;
+        int end = nums.length - 1;
         int[] result = new int[2];
 
-        while (pointer_one < pointer_two){
-         int sum = nums[pointer_one] + nums[pointer_two];
-        // checks sum is equal to target, if yes, puts values in results array, if no and less, pointerOne gets incremented, if no and higher, pointerTwo gets decremented.
+        while (start < end){
+         int sum = nums[start] + nums[end];
+        // checks sum is equal to target, if yes, puts values in results array, if no and less, start gets incremented, if no and higher, end gets decremented.
         if (sum == target) {
-             result[0] = pointer_one;
-             result[1] = pointer_two;
+             result[0] = start;
+             result[1] = end;
         } else if (sum < target) {
-            pointer_one++;
+            start++;
         } else {
-            pointer_two--;
+            end--;
         }
     }
 
