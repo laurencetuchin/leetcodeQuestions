0619c7604e1679b7fe55330854cbabe7247c7ca1
laurencetuchin <laurencetuchin@gmail.com>
2022-08-17 13:01:41 +1000

    Add Linear solution

--- src/problems/SquareOfSortedArray.java
+++ src/problems/SquareOfSortedArray.java
@@ -1,8 +1,12 @@
 package problems;
 
+import java.util.Arrays;
+
 public class SquareOfSortedArray {
 
     // brute force solution
+
+    public static int[] squareOfSortedArrayBruteForce(int[] nums) {
 
     int[] numSquared = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
@@ -11,7 +15,36 @@ public class SquareOfSortedArray {
 
         Arrays.sort(numSquared);
         return numSquared;
+    }
+
+    // two pointer solution
+
+    public static int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+        // starting iteration on right side, compares right side to left side bc already sorted
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (Math.abs(nums[left]) > nums[right]) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return result;
+
+    }
 
 
+
+    public static void main(String[] args) {
+        int[] input = new int[]{-4,1,0,3,10};
+        System.out.println(Arrays.toString(squareOfSortedArrayBruteForce(input)));
+
+        int[] numsInput = new int[]{-5,-1,0,1,4,6};
+        System.out.println(Arrays.toString(sortedSquares(numsInput)));
+    }
 
 }
